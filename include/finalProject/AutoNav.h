0c9afd156972b407e15c826cefaa1f381a71cc8c
Sam <samuelasquith@btinternet.com>
2020-02-18 13:42:57 +0000

    New version

--- include/finalProject/AutoNav.h
+++ include/finalProject/AutoNav.h
@@ -30,10 +30,7 @@ class autodrive{
         bool init();
         bool controlloop();
         void debug();
-        void loopAllowableVelocities(float min, float max, std::vector<float> &allowable);
-
-        float getAccel();
-        float calculateBrakingDistance(float velocity);
+       
     private:
         //Node Handler
         ros::NodeHandle nh_;
@@ -46,43 +43,10 @@ class autodrive{
         ros::Subscriber odom_sub_;
 
         // Variables
-        float linear_velocity = 0.0;
-        float prev_linear_velocity;
-        float angular_velocity = 0.0;
-        float prev_angular_velocity;
-
-        double escape_range_;
-        double forward_distance_;
-        double side_distance_;
-
-        std::vector<double> scan_data_; //-90, -67, -45, -22, 0, 22, 45, 67, 90
-        
-
-        float linear_acceleration = 0.1;
-        float angular_acceleration = 0.1;
-
-        double current_pose_;
-        double previous_pose_;
-
-        float allowable_V[2];
-        float allowable_W[2];
-        std::vector<float> allowableRangeLinear;
-        std::vector<float> allowableRangeAngular;
-
-        ros::Duration Time_Difference; //Used to compute max/min velocity
-
-        ros::Time current_Time;
-        ros::Time previous_Time;
-
-        double TimeSlice;
+       
 
         void laserMsgCallBack(const sensor_msgs::LaserScan::ConstPtr& msg);
         void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr& msg);
-    
-
-        float allowableMax(double currentVelocity, double acceleration);
-        float allowableMin(double currentVelocity, double acceleration);  
-
-        float findDist(float Linear, float Angular, std::vector<double> scan_data);      
+   
 };
 #endif
